3c1640a878822490daf664bbdd898cf9a2de17b9
Tayal-S <68942046+Tayal-S@users.noreply.github.com>
2021-10-24 01:03:10 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as file:
 
 
 setuptools.setup(
-	name = 'preprocess_shivani_t', #this should be unique
+	name = 'preprocess_nlp-text', #this should be unique
 	include_package_data=True,
 	version = '0.1.3',
 	author = 'Shivani tayal',
